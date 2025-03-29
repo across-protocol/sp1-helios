e19719db116d2f752b8a89cbe79cad10f179cdd5
Ihor Farion <ihor@umaproject.org>
2025-03-28 20:14:57 -0700

    update spacing in script/build.rs
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- script/build.rs
+++ script/build.rs
@@ -3,12 +3,12 @@ use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
     // build_program_with_args(
-    //     "../program",
-    //     BuildArgs {
-    //         docker: true,
-    //         elf_name: Some("sp1-helios-elf".to_string()),
-    //         output_directory: Some("../elf".to_string()),
-    //         ..Default::default()
-    //     },
-    // );
+    //    "../program",
+    //    BuildArgs {
+    //        docker: true,
+    //        elf_name: Some("sp1-helios-elf".to_string()),
+    //        output_directory: Some("../elf".to_string()),
+    //        ..Default::default()
+    //    },
+    //);
 }
