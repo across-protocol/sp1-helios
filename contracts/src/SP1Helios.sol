e792c63e6a300838b9aa66de253780381410c5ce
Ihor Farion <65650773+grasphoper@users.noreply.github.com>
2025-04-16 12:34:40 -0700

    improve: use a fixed solidity version: 0.8.28 (#17)
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- contracts/src/SP1Helios.sol
+++ contracts/src/SP1Helios.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity 0.8.28;
 
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 import {AccessControlEnumerable} from "@openzeppelin/access/extensions/AccessControlEnumerable.sol";
