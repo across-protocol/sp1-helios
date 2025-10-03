ac09626082de7f4b8886563f0e9d23a09e98e54e
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-10-03 09:37:50 -0400

    Remove docker

--- script/build.rs
+++ script/build.rs
@@ -6,7 +6,7 @@ fn main() {
         BuildArgs {
             // We use `docker: true` for reproducible builds and tag to the latest included sp1-build
             // dependency from `Cargo.toml`. https://docs.succinct.xyz/docs/sp1/writing-programs/compiling
-            docker: true,
+            docker: false,
             tag: "v5.2.1".into(),
             elf_name: Some("sp1-helios-elf".to_string()),
             output_directory: Some("../elf".to_string()),
