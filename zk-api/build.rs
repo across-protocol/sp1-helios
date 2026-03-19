a10fd86e53deb64fc072619e59706c35b0b6c28e
Taylor Webb <tbwebb22@gmail.com>
2026-03-19 09:17:12 -0600

    add tag back to buildArgs

--- zk-api/build.rs
+++ zk-api/build.rs
@@ -7,6 +7,7 @@ fn main() {
             // The SP1 v6.0.2 Docker image is x86_64-only, so docker builds don't work on ARM Macs.
             // Reproducible ELF builds are verified in CI via elf.yml (--docker --tag v6.0.2).
             docker: true,
+            tag: "v6.0.2".into(),
             elf_name: Some("sp1-helios-elf".to_string()),
             output_directory: Some("../elf".to_string()),
             ..Default::default()
