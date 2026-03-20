9a21fe0e62ab098e1f86bc3bf5aa40de60db5fb1
Taylor Webb <tbwebb22@gmail.com>
2026-03-20 16:30:13 -0600

    restore original comment on building with docker

--- zk-api/build.rs
+++ zk-api/build.rs
@@ -4,8 +4,8 @@ fn main() {
     build_program_with_args(
         "../program",
         BuildArgs {
-            // The SP1 v6.0.2 Docker image is x86_64-only, so docker builds don't work on ARM Macs.
-            // Reproducible ELF builds are verified in CI via elf.yml (--docker --tag v6.0.2).
+            // We use `docker: true` for reproducible builds and tag to the latest included sp1-build
+            // dependency from `Cargo.toml`. https://docs.succinct.xyz/docs/sp1/writing-programs/compiling
             docker: true,
             tag: "v6.0.2".into(),
             elf_name: Some("sp1-helios-elf".to_string()),
